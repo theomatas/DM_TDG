1447b89a72b7f5ea8161c7b4f4a0e4c773a7903c
theomatas <32847860+theomatas@users.noreply.github.com>
2018-09-24 10:44:59 +0200

    Update Partie_1.py

--- Partie_1.py
+++ Partie_1.py
@@ -3,7 +3,17 @@ import matrix
   
 
 A = matrix.Graph("tests.txt")
-print ("exo 1:", A.get_brut() )
+print ("exo 1: text parser", A.get_brut() )
 
-print ("exo 2:", A.get_matrix() )
+print ("exo 2: matrice", A.get_matrix() )
 
+print ("exo 3: cycle =", A.cycle() )
+
+print()
+
+B = matrix.Graph("t.txt")
+print ("exo 1: text parser", A.get_brut() )
+
+print ("exo 2: matrice", A.get_matrix() )
+
+print ("exo 3: cycle =", A.cycle() )
