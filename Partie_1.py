344c3936f1d4b62cad8364b8eddbf894548ff2c3
theomatas <32847860+theomatas@users.noreply.github.com>
2018-09-24 09:15:10 +0200

    Update Partie_1.py

--- Partie_1.py
+++ Partie_1.py
@@ -1,1 +1,58 @@
 print("partie_1" + '\n' )b #partie_1
+
+def my_open(filename):
+
+  path = filename
+  days_file = open(path,'r')
+  days = days_file.read()
+  return (days)
+
+def my_write(filename,text):
+
+  path = filename
+  days_file = open(path,'w')
+  days_file.write(text)
+  return 1
+
+def parse(text):
+  M = cut(text,'\n')
+  x = 0
+  while x < len(M):
+    if x < 2:
+      M[x] = int(M[x])
+    else:
+      M[x] = mini_parse(M[x])
+    x += 1
+  return M 
+
+def cut(text,car):
+  M = []
+  s = ""
+  for i in text:
+    if i == car:
+      M.append(s)
+      s = ""
+    else:
+      s += i
+  M.append(s)
+  return M
+
+def mini_parse(text):
+  
+  M = cut(text,' ')
+  N = []
+  for i in M:
+    N.append(int(i))
+  return N
+
+
+
+
+
+class Graph:
+  def __init__(self,filename):
+    self.brut = parse(my_open(filename))
+
+  
+
+Graph("tests.txt")
