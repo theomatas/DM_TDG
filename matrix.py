20b9e1113c52a18b767b9ccf0607ea9c0dee1560
theomatas <32847860+theomatas@users.noreply.github.com>
2018-09-24 10:44:40 +0200

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -51,7 +51,7 @@ def creat_matrix(n,lst):
     M[i[0]][i[1]] = i[2]
   return M
 
-
+   
 
 
 class Graph:
@@ -62,3 +62,31 @@ class Graph:
     return self.matrix
   def get_brut(self):
     return self.brut
+  def cycle(self):
+    M = self.matrix
+    N = []
+    x = 0
+    while x < len(M):
+      y = 0
+      while y < len(M[x]):
+        if M[x][y] != None:
+          D = 0
+          for i in [[1,1],[-1,1],[1,-1],[-1,-1]]:
+            s = 0
+            pos = [x,y]
+            
+            while s == 0:
+              
+                pos[0] += i[0]
+                pos[1] += i[1]
+                if  0 <= pos[1] < len(M[x]) and 0 <= pos[0] < len(M):
+                  if M[pos[0]][pos[1]] != None:
+                    D += 1
+                else:
+                  s = 1
+            N.append(D)
+        y += 1
+      x += 1
+    if max(N) > 1: 
+      return True
+    return False
