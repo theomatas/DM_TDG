3b1d95cfc5184741dbde626c2de9e25b6db9daa3
theomatas <32847860+theomatas@users.noreply.github.com>
2018-09-24 10:01:07 +0200

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,64 @@
+def my_open(filename):
+
+  path = filename
+  days_file = open(path,'r')
+  days = days_file.read()
+  return (days)
+
+def my_write(filename,text):
+
+  path = filename
+  days_file = open(path,'w')
+  days_file.write(text)
+  return 1
+
+def parse(text):
+  M = cut(text,'\n')
+  x = 0
+  while x < len(M):
+    if x < 2:
+      M[x] = int(M[x])
+    else:
+      M[x] = mini_parse(M[x])
+    x += 1
+  return M 
+
+def cut(text,car):
+  M = []
+  s = ""
+  for i in text:
+    if i == car:
+      M.append(s)
+      s = ""
+    else:
+      s += i
+  M.append(s)
+  return M
+
+def mini_parse(text):
+  
+  M = cut(text,' ')
+  N = []
+  for i in M:
+    N.append(int(i))
+  return N
+
+def creat_matrix(n,lst):
+  M = []
+  for i in range(n):
+    M.append([None]*n)
+  for i in lst:
+    M[i[0]][i[1]] = i[2]
+  return M
+
+
+
+
+class Graph:
+  def __init__(self,filename):
+    self.brut = parse(my_open(filename))
+    self.matrix = creat_matrix(self.brut[0],self.brut[2:])
+  def get_matrix(self):
+    return self.matrix
+  def get_brut(self):
+    return self.brut
