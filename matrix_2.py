b38e27610b4b8f44dcedd866e6d1c5826ff98065
theomatas <32847860+theomatas@users.noreply.github.com>
2018-09-27 00:04:33 +0200

    Add files via upload

--- matrix_2.py
+++ matrix_2.py
@@ -70,7 +70,28 @@ def move_rang(M,start,val,rangs):
     rangs[start] = val
   for i in M[start]:
     if i != None:
-      rangs = move_rang(M,x,val + 1,rangs)
+      rangs = move_rang(M,x,val + M[start][x],rangs)
+    x += 1
+  return rangs
+
+def move_early(M,start,val,rangs):
+  x = 0
+  if rangs[start] < val:
+    rangs[start] = val
+  for i in M[start]:
+    if i != None:
+      rangs = move_early(M,x,val + M[start][x],rangs)
+    x += 1
+  return rangs
+
+def move_late(M,start,val,rangs):
+  x = 0
+  m = len(M)
+  if rangs[start] < val:
+    rangs[start] = val
+  while x < m:
+    if M[x][start] != None:
+      rangs = move_late(M,x,val - M[x][start],rangs)
     x += 1
   return rangs
 
@@ -80,6 +101,66 @@ def is_inside(M,x):
       return True
   return False
 
+def in_out(M):
+  m = len(M)
+  in_ = [0]*m
+  out_ = [0]*m
+  x = 0
+  while x < m:
+    y = 0
+    s = 0
+    while y < m:
+      if M[y][x] != None:
+        s += 1
+      y += 1
+    if s == 0:  
+      in_[x] = 1
+    x += 1
+  x = 0
+  while x < m:
+    y = 0
+    s = 0
+    while y < m:
+      if M[x][y] != None:
+        s += 1
+      y += 1
+    if s == 0:  
+      out_[x] = 1
+    x += 1  
+  return [in_,out_]
+      
+def in_zero(M):
+  in_ = in_out(M)[0].index(1)
+  m = len(M)
+  for i in M[in_]:
+    if i != 0 and i != None:
+      return False
+  return True
+
+def check_incident(M):
+  m = len(M)
+  x = 0
+  while x < m:
+    y = 0
+    s = None
+    while y < m:
+      j = M[x][y]
+      if j != None and s != None and s != j:
+        return False
+      if j != None:
+        s = j
+      y += 1
+    x += 1
+  return True
+
+def copy(M):
+  N = []
+  for i in M:
+    N.append(i)
+  return N
+
+
+    
 class Graph:
   def __init__(self,filename):
     self.brut = parse(my_open(filename))
@@ -100,22 +181,70 @@ class Graph:
     M = self.matrix
     m = self.brut[0]
     rangs = [1000]*m
+    in_ = in_out(M)[0]
     x = 0
-    while x < m:
-      y = 0
-      s = 0
-      while y < m:
-        if M[y][x] != None:
-          s += 1
-        y += 1
-      if s == 0:
-        rangs[x] = 0
+    for i in in_:
+      if i == 1:
         rangs = move_rang(M,x,0,rangs)
-      x += 1
+        x += 1
     return rangs
+  def ordonnance(self):
+    M = self.matrix
+    in_ = in_out(M)[0]
+    out_ = in_out(M)[1]
+    if self.cycle():
+      return False , "is a cycle"
+    if not sum(in_) == 1 :
+      return False , "wrong number of input"
+    if not sum(out_) == 1:
+      return False , "wrong number of output"  
+    if not in_zero(M):
+      return False , "not 0 for out of first node"
+    if not check_incident(M):
+      return False , "incident arc value not equal" 
+    return True
+  def trajet_early(self):
+    M = self.matrix
+    m = self.brut[0]
+    early = [0]*m
+    in_ = in_out(M)[0]
+    x = 0
+    for i in in_:
+      if i == 1:
+        early = move_early(M,x,0,early)
+        x += 1
+    return early  
+  def trajet_late(self,late):
+    M = self.matrix
+    m = self.brut[0]
+    out_ = in_out(M)[1]
+    x = 0
+    for i in out_:
+      if i == 1:
+        late = move_late(M,x,late[x],late)
+      x += 1
+    return late   
+  def tache(self):
+    M = self.matrix
+    N = []
+    for i in M:
+      N.append(max(i))
+    return N
+  def trajet(self):
+    if self.ordonnance() != True:
+      return False    
+    early = self.trajet_early()
+    late = self.trajet_late(copy(early))
+    tache = self.tache()
+    marge = []
+    for i in range(len(self.matrix)):
+      e = early[i]
+      l = late[i]
+      t = tache[i]
+      if t != None:
+        marge.append(l-e-t)
+      else:
+        marge.append(0)
         
-        
-      
-      
-      
     
+    return " early = " + str(early) + " late = " + str(late) + " marge = " + str(marge)
